48f3bae5aef6d2703ef4872510bcbcb79715e80e
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-08 18:41:30 +0100

    extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(stderr, text, sizeof(text));
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r\n";
+write(1, text, sizeof(text));
 return (1);
 }
