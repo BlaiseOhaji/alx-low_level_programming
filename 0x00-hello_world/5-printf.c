5712cc1a95e46e3ff987a0a7f495a65114d9a2ce
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-08 16:41:22 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h>                                                                                                                      
-
-                                                                                                                                        
-
-int main (void) {                                                                                                                       
-
-                                                                                                                                        
-
+#include <stdio.h>                                                                                                                         
+/**
+ * main - main function block
+ * Return: 0
+ */
+int main(void) 
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
 }
