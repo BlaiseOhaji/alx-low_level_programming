1bf4f2e90d2868e00eda43080ae3464321b3b7a8
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-08 18:31:52 +0100

    extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ *main - main function
+ *Return: 1
+ */
 int main(void)
 {
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, text, sizeof(text));
+return (1);
+}
