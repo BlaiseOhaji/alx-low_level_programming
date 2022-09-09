384389fccdf9fcba982cc7c8763fa92b7930b919
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 17:09:25 +0100

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,24 @@
-sd
+#include <stdio.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+
+c = 'a';
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
