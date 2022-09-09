4dd25b8dba2e83e40bcd0aa00289ca7c4ccb60e8
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 17:13:19 +0100

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,20 @@
-sd
+#include <stdio.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+
+while (i < 9)
+{
+putchar(48 + i);
+putchar(", ");
+i++;
+}
+putchar(48 + i);
+putchar('\n');
+return (0);
+}
