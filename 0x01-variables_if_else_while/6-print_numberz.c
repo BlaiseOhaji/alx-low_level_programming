7d27a48050824c6f7d8acb346d87735729a1f9c8
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 16:50:31 +0100

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
-sda
+#include <stdio.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+
+while (i <= 9)
+{
+putchar(i);
+c++;
+}
+
+putchar('\n');
+return (0);
+}
