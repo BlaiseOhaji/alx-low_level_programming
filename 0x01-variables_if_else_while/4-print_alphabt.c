62f7e2f99a8042755b1f4a7adbe4e02ef69bf006
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 16:39:48 +0100

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
-sd
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+if (c != e && c != q)
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
