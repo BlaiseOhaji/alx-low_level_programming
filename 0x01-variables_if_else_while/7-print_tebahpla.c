89f775b1a61e643debdfbc81b2e83907fbb8a0a5
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 17:06:01 +0100

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
-sd
+#include <stdio.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+
+putchar('\n');
+return (0);
+}
