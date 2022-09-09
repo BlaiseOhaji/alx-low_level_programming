4cfeea404700970952beb87c95d57bf72445c12e
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 18:15:28 +0100

    extra

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,36 @@
-sd
+#include <stdio.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+int a = 0;
+int b;
+
+while (a < 10)
+{
+b = 0;
+while (b < 10)
+{
+if (a != b && a < b)
+{
+putchar(a);
+putchar(b);
+
+if (a + b != 17)
+{
+putchar(a);
+putchar(b);
+}
+ 
+}
+b++;
+
+}
+a++
+}
+putchar('\n');
+return (0);
+}
