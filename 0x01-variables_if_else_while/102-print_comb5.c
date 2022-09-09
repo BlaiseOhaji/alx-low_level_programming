6260e4c42231ef6807238bb9925e0c71fee36d6e
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-09 19:49:47 +0100

    extra

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,50 @@
-sd
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function block
+ * Return: 0
+ */
+
+int main(void)
+{
+int a = 0;
+int b;
+int c;
+int d;
+
+while (a < 10)
+{
+b = 0;
+while (b < 10)
+{
+c = 0;
+while (c < 10)
+{
+d = 0;
+while (d < 10)
+{
+if (!(a == d && b == c))
+{
+putchar(48 + a);
+putchar(48 + b);
+putchar(' ');
+putchar(48 + c);
+putchar(48 + d);
+
+if (a + b + c d != 35)
+{
+putchar(',');
+putchar(' ');
+}
+}
+d++;
+}
+c++;
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
