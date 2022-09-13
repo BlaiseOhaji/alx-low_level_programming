43cfc96df9dde7baaca75138b95a5677f8d11ef9
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 16:19:36 +0100

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
-jk
+#include "main.h"
+/**
+ * main - print_alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
