394abefd3b5e4355de5d3db0b08a3273868006b3
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 16:47:34 +0100

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,16 @@
-sd
+#include "main.h"
+/**
+ * _islower - main function
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+  if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
