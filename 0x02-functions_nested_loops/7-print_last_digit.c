6faf1a1fbde3110c74ed6b49149bffe7ef456d3b
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 18:24:06 +0100

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,25 @@
-sd
+#include "main.h"
+/**
+ * print_last_digit - main function
+ * @n : number to be worked on
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
+}
