e2c92fe5fc5ff9ed85b305eaddee8d229de4248d
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 18:06:29 +0100

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * _abs - main function
+ * @n : number to be worked on
  *Return: Always 0
  */
 int _abs(int n)
 {
-  if (n < 0)
-  {
-    return (-n);
-  }
-  else
-  {
-    return (n);
-  }
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
 }
