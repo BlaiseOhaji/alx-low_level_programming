65a329e5f89fe1931469db4b3dcb52d855b8e80f
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 17:10:40 +0100

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 /**
  * print_sign - main function
  * @n : number to be checked
- * Return : 1 or 0
+ * Return: 1 or 0
  */
 int print_sign(int n)
 {
 if (n > 0)
 	{
 		_putchar('+');
-    return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-    return(0);
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-    return(-1);
+		return (-1);
 	}
 }
