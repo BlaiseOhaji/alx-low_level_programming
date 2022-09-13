4f130285d32c16a51da52cb1392ed9ddc6970f0a
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 16:54:34 +0100

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,18 @@
-sd
+#include "main.h"
+/**
+ * _isalph - main function
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
