c7171377f2d95f933423a60f3122357ac98cce7c
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-13 17:29:34 +0100

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,17 @@
-sd
+#include "main.h"
+/**
+ * _abs - main function
+ *Return: Always 0
+ */
+int _abs(int n)
+{
+  if (int n < '0')
+  {
+    _putchar(-n);
+  }
+  else
+  {
+    _putchar(n);
+  }
+  return (0);
+}
