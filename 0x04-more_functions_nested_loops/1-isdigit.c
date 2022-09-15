11feb5f08322aca20ed8270c3354c290ca81f554
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-15 15:55:14 +0100

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,17 @@
-sd
+#include "main.h"
+/**
+ * _isdigit - checks for digits
+ * @c: parameter to be checked
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
