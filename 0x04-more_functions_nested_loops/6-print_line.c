b6caa96bc1679ba3da49230bc4025033ed6251e8
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-15 16:22:30 +0100

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,16 @@
-sd
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: parameter to be used
+ */
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
