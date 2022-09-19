077e54ba0b2c6c8123a2da5a5469f482052a1e20
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-19 14:54:11 +0100

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,19 @@
-sd
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints array
+ * @a: parameters
+ * @n: parameters
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
