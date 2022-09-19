f4de5bbab25f68fbd0c6c1cfc27f54453ec5726e
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-19 14:37:25 +0100

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,24 @@
-sd
+#include "main.h"
+/**
+ * puts_half - prints half of a string.
+ * @str: parameter
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (count >= 0)
+	{
+		if (str[count] != '\0')
+		count++;
+	}
+
+	if (count % 2 == 0)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
