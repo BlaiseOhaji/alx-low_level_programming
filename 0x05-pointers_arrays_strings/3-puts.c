d81adb291eb6a25b3d393bcfe67c2c29b1b209dc
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-19 13:35:30 +0100

    4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
-cs
+#include "main.h"
+/**
+ * _puts - puts to stdout
+ * @str: parameter
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
