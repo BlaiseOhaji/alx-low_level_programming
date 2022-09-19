667752944d0e25c63428f308d81a8a3acd055cb9
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-19 14:42:13 +0100

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,12 @@ void puts_half(char *str)
 
 	while (count >= 0)
 	{
-		if (str[count] != '\0')
+		if (str[count] == '\0')
+			break;
 		count++;
 	}
 
-	if (count % 2 == 0)
+	if (count % 2 == 1)
 		i = count / 2;
 	else
 		i = (count - 1) / 2;
