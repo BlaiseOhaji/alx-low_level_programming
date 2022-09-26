2bb10c4f03d0273460c616a333c027491bc10a56
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-26 14:22:28 +0100

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,21 @@
+#include "main.h"
+/**
+ *_memcpy - copy bytes from one address to another address "to"
+ *@dest: content to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
 
-sd
+	return (dest);
+}
