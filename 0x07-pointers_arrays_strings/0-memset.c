3860f7eb3fc23d98b4550ead960d07a95d4e4da4
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-26 13:45:47 +0100

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,19 @@
-sd
+#include "main.h"
+/**
+ *_memset - main function
+ *@s: pointer of memory to fill
+ *@b: value to be set
+ *@n: no bytes of the memory
+ *Return: dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
