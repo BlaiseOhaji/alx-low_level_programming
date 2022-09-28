62860189ce67204de2d3e39ff383df95824a990b
BlaiseOhaji <111230368+BlaiseOhaji@users.noreply.github.com>
2022-09-28 12:50:02 +0100

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,17 @@
-sd
+#include "main.h"
+/**
+ *factorial - gets the factorial of any given number
+ *@n: input number
+ *Return: factorial as an int
+ */
+int factorial(int n)
+{
+if (n == 0)
+return (1);
+
+else if (n < 0)
+return (-1);
+
+else return (n * factorial(int n - 1));
+
+}
